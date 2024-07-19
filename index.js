a4e8764786454e1c448964041d0efa0f3544e2bc
GunterHamilton <colevergaming@gmail.com>
2024-07-19 01:57:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -59,8 +59,12 @@ client.on('interactionCreate', async interaction => {
   try {
     await command.execute(interaction);
   } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    console.error('Error handling interaction:', error);
+    if (!interaction.deferred && !interaction.replied) {
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    } else {
+      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
   }
 });
 

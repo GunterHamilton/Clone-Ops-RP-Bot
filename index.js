d1cae73aedce38575c11e20e49f3b33ca3cd0a26
GunterHamilton <colevergaming@gmail.com>
2024-07-19 00:53:28 -0400

    Version 1.0

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.commands = new Collection();
+
+// Command handling
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+// Event handling
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+// Database connection
+require('./config/database');
+
+// Login to Discord
+client.login(process.env.TOKEN);

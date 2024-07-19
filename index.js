4e48bca5b11d93a1f0cdbbb19559c334ef406d5b
GunterHamilton <colevergaming@gmail.com>
2024-07-19 01:08:11 -0400

    Version 1

---
+++ index.js
@@ -0,0 +1,31 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.once('ready', async () => {
+    const data = client.commands.map(cmd => ({ name: cmd.name, description: cmd.description }));
+    await client.application.commands.set(data);
+});
+
+client.login(process.env.TOKEN);
